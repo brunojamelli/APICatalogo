324cbbfd4c76572690fb1bc498d3575acfecefa1
brunojamelli <brunojamelli@ufrn.edu.br>
2024-07-21 23:08:27 -0300

    adição de models para o Identity

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+public class RegisterModel
+{
+    [Required(ErrorMessage = "Username is required")]
+    public string? Username { get; set; }
+
+    [EmailAddress]
+    [Required(ErrorMessage = "Email is required")]
+    public string? Email { get; set; }
+
+    [Required(ErrorMessage = "Password is required")]
+    public string? Password { get; set; }
+}
