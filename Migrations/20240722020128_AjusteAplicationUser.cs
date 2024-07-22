2c1eacb6794b63aaa6b8cf045fe0490b3b2f0dfa
brunojamelli <brunojamelli@ufrn.edu.br>
2024-07-21 23:03:13 -0300

    adição de migration para uso do AplicationUser

---
+++ Migrations/20240722020128_AjusteAplicationUser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AjusteAplicationUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RefresToken",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RefresTokenExpiryTime",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RefresToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "RefresTokenExpiryTime",
+                table: "AspNetUsers");
+        }
+    }
+}
